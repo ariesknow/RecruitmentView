61946036ce9b4bf7fe8188f8f1cdddf3bc733016
Yuichiro Tsuji <[t.yuyu.alies@gmail.com]>
2018-05-17 15:02:41 +0900

    add CollectionView

--- RecruitmentApp/RecruitMentViewController.swift
+++ RecruitmentApp/RecruitMentViewController.swift
@@ -7,12 +7,21 @@
 //
 
 import UIKit
+import VegaScroll
 
 class RecruitMentViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let layout = VegaScrollFlowLayout()
+        collectionView.collectionViewLayout = layout
+        layout.minimumLineSpacing = 20
+        layout.itemSize = CGSize(width: collectionView.frame.width, height: 87)
+        layout.sectionInset = UIEdgeInsets(top: 10, left: 0, bottom: 10, right: 0)
     }
 
     override func didReceiveMemoryWarning() {
